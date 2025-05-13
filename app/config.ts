c8b909c8c6fdf04a34af950db6ab06be424f0cc6
ke <fitenne@qq.com>
2025-05-13 18:32:36 +0800

    fix: API_BASE

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,4 @@
 export const APP_BASE: string = import.meta.env.VITE_APP_BASE_URL;
-export const API_BASE = new URL("/api/v1", APP_BASE);
+export const API_BASE = import.meta.env.DEV
+  ? new URL("/api/v1", APP_BASE)
+  : "http://localhost:8080/api/v1";
